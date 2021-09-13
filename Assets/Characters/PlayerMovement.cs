11377268c528bb36e60ab1f97cc99a55859d0134
AMKilgore <albert.kilgore511@topper.wku.edu>
2021-09-13 10:48:53 -0500

    Added collision triggers to detect whether the player is grounded. Also added a very basic jump script to player, need to work on jump height.

--- Assets/Characters/PlayerMovement.cs
+++ Assets/Characters/PlayerMovement.cs
@@ -7,8 +7,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float horizontalSpeed = 3.4f;
+    public float verticalSpeed = 50.0f;
+    public bool isGrounded = true;
     float horizontalDirection;
-    float gravityScale = 2.0f;
+    float verticalDirection;
+    float gravityScale = 1.0f;
     Rigidbody2D r2d;
 
     // Start is called before the first frame update
@@ -28,12 +31,41 @@ public class PlayerMovement : MonoBehaviour
         {
             horizontalDirection = Input.GetKey(KeyCode.LeftArrow) ? -1 : 1;
         }
+        else
+        {
+            horizontalDirection = 0;
+        }
+
+        // Vertical movement
+        if (Input.GetKey(KeyCode.Z) && isGrounded)
+        {
+            verticalDirection = 1;
+            r2d.AddForce(Vector2.up * 50.0f);
+        }
     }
 
     void FixedUpdate()
     {
         // Calculate movement velocity
         //$$ 0 is a placeholder on the y axis movement
-        r2d.velocity = new Vector2((horizontalDirection) * horizontalSpeed, 0);
+        //r2d.velocity = new Vector2((horizontalDirection) * horizontalSpeed, 0);
+        //r2d.MovePosition(new Vector2((horizontalDirection * horizontalSpeed * Time.deltaTime) + r2d.position.x,
+        //    (verticalDirection * verticalSpeed * Time.deltaTime/4) + r2d.position.y));
+
+        //r2d.MovePosition(new Vector2((horizontalDirection * horizontalSpeed * Time.deltaTime) + r2d.position.x,
+        //    r2d.position.y));
+        r2d.transform.Translate(Vector2.right * horizontalDirection * horizontalSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isGrounded = true;
+        Debug.Log("grounded");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+        Debug.Log("not grounded");
     }
 }
