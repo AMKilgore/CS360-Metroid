684fb80a3969d1292f77c6be73a1b041fc217543
AMKilgore <52934543+AMKilgore@users.noreply.github.com>
2021-10-11 07:37:07 -0500

    Added third map (long hallway) I think this is repeated quite often. Next goal: scene transitions.

--- Assets/Scripts/CameraLockedX.cs
+++ Assets/Scripts/CameraLockedX.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class CameraLockedX : MonoBehaviour
 {
+    private float xPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform cameraPos = gameObject.GetComponent(typeof(Transform)) as Transform;
+        xPos = cameraPos.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         Transform playerPos = gameObject.GetComponentInParent(typeof(Transform)) as Transform;
-        this.transform.position = new Vector3(5.97f, playerPos.position.y, -10.0f);
+        this.transform.position = new Vector3(xPos, playerPos.position.y, -10.0f);
     }
 
     private void LateUpdate()
     {
-        this.transform.position = new Vector3(5.97f, this.transform.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(xPos, this.transform.position.y, this.transform.position.z);
     }
 }
--- Assets/Scripts/CameraLockedY.cs
+++ Assets/Scripts/CameraLockedY.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class CameraLockedY : MonoBehaviour
 {
+    private float yPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform cameraPos = gameObject.GetComponent(typeof(Transform)) as Transform;
+        yPos = cameraPos.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         Transform playerPos = gameObject.GetComponentInParent(typeof(Transform)) as Transform;
-        this.transform.position = new Vector3(playerPos.position.x, 1.61f, -10.0f);
+        //this.transform.position = new Vector3(playerPos.position.x, 1.61f, -10.0f);
+        this.transform.position = new Vector3(playerPos.position.x, yPos, -10.0f);
     }
 
     private void LateUpdate()
     {
-        this.transform.position = new Vector3(this.transform.position.x, 6.5f, this.transform.position.z);
+        //this.transform.position = new Vector3(this.transform.position.x, 6.5f, this.transform.position.z);
+        this.transform.position = new Vector3(this.transform.position.x, yPos, this.transform.position.z);
     }
 
 }
