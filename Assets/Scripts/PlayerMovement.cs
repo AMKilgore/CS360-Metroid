50a07cd335ac52bba42dbbde5099a9612ba9234f
AMKilgore <52934543+AMKilgore@users.noreply.github.com>
2021-10-20 21:13:23 -0500

    Added flip jumping, need to find if player is grounded.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 [RequireComponent(typeof(Rigidbody2D))]
 
@@ -13,18 +14,25 @@ public class PlayerMovement : MonoBehaviour
     public bool isGrounded = true;
     float horizontalDirection;
     float playerFacing;
-    float speed;
     float verticalDirection;
     float gravityScale = 1.0f;
-    Rigidbody2D r2d;
 
+    Rigidbody2D r2d;
+    BoxCollider2D boxCollider2D;
     Collider2D lastTouchedCollider;
+
+    private Tilemap tilemap;
+    TilemapCollider2D a;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         r2d = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+
+        a = GameObject.Find("Brinstar_Tiles").GetComponent<TilemapCollider2D>();
+
         r2d.freezeRotation = true;
         r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
         r2d.gravityScale = gravityScale;
@@ -46,19 +54,20 @@ public class PlayerMovement : MonoBehaviour
             horizontalDirection = 0;
         }
 
-        /*
-        if (horizontalDirection != 0)
-        {
-            playerFacing = horizontalDirection;
-        }
-        */
 
         // Vertical movement
         if (Input.GetKey(KeyCode.Z) && isGrounded)
         {
             verticalDirection = 1;
             r2d.AddForce(Vector2.up * 10.0f);
+            
         }
+        else if (isGrounded)
+        {
+            verticalDirection = 0;
+        }
+
+        Debug.Log(isGroundedTest());
     }
 
     void FixedUpdate()
@@ -75,6 +84,15 @@ public class PlayerMovement : MonoBehaviour
             animator.SetFloat("Direction", playerFacing);
             animator.SetBool("IsMoving", false);
         }
+
+        if (verticalDirection != 0)
+        {
+            animator.SetBool("IsJumping", true);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -82,7 +100,6 @@ public class PlayerMovement : MonoBehaviour
         if (other.tag == "Ground")
         {
             isGrounded = true;
-            Debug.Log("grounded");
         }
 
         lastTouchedCollider = other;
@@ -100,5 +117,17 @@ public class PlayerMovement : MonoBehaviour
 
             lastTouchedCollider = null;
         }
+    }
+
+    private bool isGroundedTest()
+    {
+        RaycastHit2D rh = Physics2D.Raycast(transform.position, Vector2.down, transform.position.y + 0.1f, 10);
+        RaycastHit2D[] l = new RaycastHit2D[1];
+        ContactFilter2D cf = new ContactFilter2D();
+        int c = a.Raycast(Vector2.up, l, 0.5f);
+        Debug.Log(c);
+        Debug.Log(rh.collider);
+
+        return Physics2D.Raycast(boxCollider2D.bounds.center, Vector2.down, boxCollider2D.bounds.extents.y + 0.1f, 10);
     }
 }
